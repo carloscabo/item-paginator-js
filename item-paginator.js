dd96dab22198473c195dc14c9c075a2b96bd84d6
Carlos Cabo <carloscabo@gmail.com>
2015-12-13 14:53:17 +0100

    Converting to constructor

--- item-paginator.js
+++ item-paginator.js
@@ -9,37 +9,43 @@
   });
 */
 
-var itemPaginator = {
-  // Settings
-  set: {
-    items: 0,
-    pages: 0,
-    offset: 0, // Useful
-    limit: 0,  // Useful
-    pages_to_show: 7,
-    items_per_page: 0,
-    items_last_page: 0,
-    current_page: 0,
-    container: null,
-    text: {
-      ellipsis: '…',
-      next: 'Next',
-      prev: 'Prev',
-    },
-    classes: {
-      current: 'active',
-      disabled: 'disabled',
-      prev: 'prev',
-      next: 'next',
-      page: 'page-',
-      paginator: 'item-paginator',
-      ellipsis: 'ellipsis'
-    },
-    $html: null
-  },
+var ItemPaginator = (function() {
 
-  init: function(user_options) {
+  function Constructor(user_options) {
+
+    // Settings
+    this.set = {
+      items: 0,
+      pages: 0,
+      offset: 0, // Useful
+      limit: 0,  // Useful
+      pages_to_show: 7,
+      items_per_page: 0,
+      items_last_page: 0,
+      current_page: 0,
+      container: null,
+      text: {
+        ellipsis: '…',
+        next: 'Next',
+        prev: 'Prev',
+      },
+      classes: {
+        current: 'active',
+        disabled: 'disabled',
+        prev: 'prev',
+        next: 'next',
+        page: 'page-',
+        paginator: 'item-paginator',
+        ellipsis: 'ellipsis'
+      },
+      $html: null
+    };
+
     this.set = $.extend(true, this.set, user_options);
+
+    if (typeof(this.set.onChangePage) == "function") {
+      this.onChangePage = this.set.onChangePage;
+    }
 
     if (this.set.items === 0 || this.set.items_per_page === 0) {
       return false; // Bad settings, do nothing
@@ -50,27 +56,29 @@ var itemPaginator = {
     }
 
     this.calculatePageCount();
-  },
+  }
 
-  calculatePageCount: function() {
-    this.set.pages = parseInt(Math.floor(this.set.items / this.set.items_per_page), 10);
-    if ( this.set.items > ( this.set.pages * this.set.items_per_page )) {
-      this.set.pages++;
-    }
-     // If there is 1 page only, no need for paginator
-    if (this.set.pages < 2) {
-      this.destroy();
-      return;
-    }
-    this.set.current_page = 1;
-    this.createHTML();
-  },
+  Constructor.prototype = {
 
-  /**
-   * Crea el HTML y lo attachea
-   */
-  createHTML: function() {
-    var
+    calculatePageCount: function() {
+      this.set.pages = parseInt(Math.floor(this.set.items / this.set.items_per_page), 10);
+      if ( this.set.items > ( this.set.pages * this.set.items_per_page )) {
+        this.set.pages++;
+      }
+      // If there is 1 page only, no need for paginator
+      if (this.set.pages < 2) {
+        this.destroy();
+        return;
+      }
+      this.set.current_page = 1;
+      this.createHTML();
+    },
+
+    /**
+    * Crea el HTML y lo attachea
+    */
+    createHTML: function() {
+      var
       that = this,
       // media
       media = Math.floor(this.set.pages_to_show / 2),
@@ -79,173 +87,179 @@ var itemPaginator = {
       // Restamos la última
       loop_fin = this.set.current_page + media - 1;
 
-    // Paginator container
-    this.set.$html = $('<div>').addClass(this.set.classes.paginator);
+      // Paginator container
+      this.set.$html = $('<div>').addClass(this.set.classes.paginator);
 
-    // Offset & limit
-    this.set.offset = (this.set.current_page - 1) * this.set.items_per_page + 1;
-    this.set.limit = this.set.items_per_page;
+      // Offset & limit
+      this.set.offset = (this.set.current_page - 1) * this.set.items_per_page + 1;
+      this.set.limit = this.set.items_per_page;
 
-    // Create ellipsis
-    if (this.set.classes.ellipsis !== false) {
-      $ellipsis = $('<span>').addClass(this.set.classes.ellipsis).text(this.set.text.ellipsis);
-    }
+      // Create ellipsis
+      if (this.set.classes.ellipsis !== false) {
+        $ellipsis = $('<span>').addClass(this.set.classes.ellipsis).text(this.set.text.ellipsis);
+      }
 
-    // Prev
-    if(this.set.text.prev !== false) {
-      var $prev = $('<span>').addClass(this.set.classes.prev).text(this.set.text.prev);
-      if (this.set.current_page < 2) {
-        $prev.addClass(this.set.classes.disabled);
+      // Prev
+      if(this.set.text.prev !== false) {
+        var $prev = $('<span>').addClass(this.set.classes.prev).html(this.set.text.prev);
+        if (this.set.current_page < 2) {
+          $prev.addClass(this.set.classes.disabled);
+        } else {
+          $prev.on('click', function(e) {
+            e.preventDefault();
+            var cp = that.set.current_page - 1;
+            that.goToPage(cp);
+          });
+        }
+        $prev.appendTo(this.set.$html);
+      }
+
+      // First page
+      var $first = $('<a href="#page-1" class="page page-first page-1">1</a>');
+      $first.appendTo(this.set.$html);
+
+      // First ellipsis
+      if (this.set.classes.ellipsis !== false && this.set.pages > this.set.pages_to_show) {
+        if (this.set.current_page > media + 1) {
+          $ellipsis.clone().appendTo(this.set.$html);
+        }
+      }
+
+      // Rest of pages
+      // Less pages than pages_to_show... no need of ellipsis, simple loop
+      if (this.set.pages < this.set.pages_to_show) {
+        for (var i = 2; i < this.set.pages; i++) {
+          var $page = $('<a href="#page-'+i+'" class="page page-last page-'+i+'">'+i+'</a>');
+          $page.appendTo(this.set.$html);
+        }
       } else {
-        $prev.on('click', function(e) {
-          e.preventDefault();
-          var cp = that.set.current_page - 1;
-          that.goToPage(cp);
-        });
-      }
-      $prev.appendTo(this.set.$html);
-    }
 
-    // First page
-    var $first = $('<a href="#page-1" class="page page-first page-1">1</a>');
-    $first.appendTo(this.set.$html);
+        // Esta cerca del principio
+        if (this.set.current_page < this.set.pages_to_show - 2) {
+          loop_ini = 2;
+          loop_fin = this.set.pages_to_show - 1;
+        }
 
-    // First ellipsis
-    if (this.set.classes.ellipsis !== false && this.set.pages > this.set.pages_to_show) {
-      if (this.set.current_page > media + 1) {
-        $ellipsis.clone().appendTo(this.set.$html);
-      }
-    }
+        // Esta cerca del final
+        if (this.set.current_page >= (this.set.pages - media)) {
+          loop_ini = this.set.pages - this.set.pages_to_show + 2;
+          loop_fin = this.set.pages - 1;
+        }
 
-    // Rest of pages
-    // Less pages than pages_to_show... no need of ellipsis, simple loop
-    if (this.set.pages < this.set.pages_to_show) {
-      for (var i = 2; i < this.set.pages; i++) {
-        var $page = $('<a href="#page-'+i+'" class="page page-last page-'+i+'">'+i+'</a>');
-        $page.appendTo(this.set.$html);
-      }
-    } else {
+        // Hay más elementos que elementos por página
+        for (var i = loop_ini; i <= loop_fin; i++) {
+          var $page = $('<a href="#page-'+i+'" class="page page-last page-'+i+'">'+i+'</a>');
+          $page.appendTo(this.set.$html);
+        }
 
-      // Esta cerca del principio
-      if (this.set.current_page < this.set.pages_to_show - 2) {
-        loop_ini = 2;
-        loop_fin = this.set.pages_to_show - 1;
       }
 
-      // Esta cerca del final
-      if (this.set.current_page >= (this.set.pages - media)) {
-        loop_ini = this.set.pages - this.set.pages_to_show + 2;
-        loop_fin = this.set.pages - 1;
+      // Final ellipsis
+      if (this.set.classes.ellipsis !== false && this.set.pages > this.set.pages_to_show) {
+        if (this.set.current_page < (this.set.pages - media)) {
+          $ellipsis.appendTo(this.set.$html);
+        }
       }
 
-      // Hay más elementos que elementos por página
-      for (var i = loop_ini; i <= loop_fin; i++) {
-        var $page = $('<a href="#page-'+i+'" class="page page-last page-'+i+'">'+i+'</a>');
-        $page.appendTo(this.set.$html);
+      // Last page
+      var $last = $('<a href="#page-'+this.set.pages+'" class="page page-last page-'+this.set.pages+'">'+this.set.pages+'</a>');
+      $last.appendTo(this.set.$html)
+
+      // Add active class
+      this.set.$html.find('a.page-'+this.set.current_page).addClass(this.set.classes.current);
+
+      // Pages events
+      this.set.$html.find('a.page').not('.'+this.set.classes.current).off('click').on('click', function(e) {
+        e.preventDefault();
+        that.goToPage(parseInt($(this).text(), 10));
+      });
+
+      // Next
+      if(this.set.text.next !== false) {
+        var $next = $('<span>').addClass(this.set.classes.next).html(this.set.text.next);
+        if (this.set.current_page === this.set.pages) {
+          $next.addClass(this.set.classes.disabled);
+        } else {
+          $next.on('click', function(e) {
+            e.preventDefault();
+            var cp = that.set.current_page + 1;
+            that.goToPage(cp);
+          });
+        }
+        $next.appendTo(this.set.$html);
       }
 
-    }
+      // Append to DOM
+      this.destroy('');
+      this.set.$html.appendTo(this.set.container);
 
-    // Final ellipsis
-    if (this.set.classes.ellipsis !== false && this.set.pages > this.set.pages_to_show) {
-      if (this.set.current_page < (this.set.pages - media)) {
-        $ellipsis.appendTo(this.set.$html);
+      // Fire events so they are called on first run also
+      if (typeof(this.set.onChangePage) == "function") {
+        this.set.onChangePage();
       }
-    }
-
-    // Last page
-    var $last = $('<a href="#page-'+this.set.pages+'" class="page page-last page-'+this.set.pages+'">'+this.set.pages+'</a>');
-    $last.appendTo(this.set.$html)
-
-    // Add active class
-    this.set.$html.find('a.page-'+this.set.current_page).addClass(this.set.classes.current);
-
-    // Pages events
-    this.set.$html.find('a.page').not('.'+this.set.classes.current).off('click').on('click', function(e) {
-      e.preventDefault();
-      that.goToPage(parseInt($(this).text(), 10));
-    });
-
-    // Next
-    if(this.set.text.next !== false) {
-      var $next = $('<span>').addClass(this.set.classes.next).text(this.set.text.next);
-      if (this.set.current_page === this.set.pages) {
-        $next.addClass(this.set.classes.disabled);
-      } else {
-        $next.on('click', function(e) {
-          e.preventDefault();
-          var cp = that.set.current_page + 1;
-          that.goToPage(cp);
-        });
+      if (this.set.current_page === 1 && typeof(this.set.onFirstPage) == "function") {
+        this.set.onFirstPage();
       }
-      $next.appendTo(this.set.$html);
-    }
+      if (this.set.current_page === this.set.pages && typeof(this.set.onLastPage) == "function") {
+        this.set.onLastPage();
+      }
+    },
 
-    // Append to DOM
-    this.destroy('');
-    this.set.$html.appendTo(this.set.container);
+    destroy: function() {
+      this.set.container.html('');
+      return;
+    },
 
-    // Fire events so they are called on first run also
-    this.onChangePage();
-    if (this.set.current_page === 1) {
-      this.onFirstPage();
-    }
-    if (this.set.current_page === this.set.pages) {
-      this.onLastPage();
-    }
-  },
+    setItems: function(items) {
+      this.set.items = items;
+      this.calculatePageCount();
+    },
 
-  destroy: function() {
-    this.set.container.html('');
-    return;
-  },
+    getCurentPage: function() {
+      return this.set.current_page;
+    },
 
-  setItems: function(items) {
-    this.set.items = items;
-    this.calculatePageCount();
-  },
+    getPages: function() {
+      return this.set.pages;
+    },
 
-  getCurentPage: function() {
-    return this.set.current_page;
-  },
+    goToPage: function(pagenum) {
+      if (pagenum > this.set.pages) {
+        pagenum = this.set.pages;
+      }
+      if ( pagenum > 0 && pagenum < this.set.pages + 1 ) {
+        this.set.current_page = pagenum;
+        this.createHTML();
+      }
+    },
 
-  getPages: function() {
-    return this.set.pages;
-  },
+    goToNext: function() {
+      if (this.set.current_page < this.set.pages) {
+        this.goToPage(this.set.current_page++);
+      }
+    },
 
-  goToPage: function(pagenum) {
-    if (pagenum > this.set.pages) {
-      pagenum = this.set.pages;
-    }
-    if ( pagenum > 0 && pagenum < this.set.pages + 1 ) {
-      this.set.current_page = pagenum;
-      this.createHTML();
-    }
-  },
+    goToPrev: function() {
+      if (this.set.current_page > 1) {
+        this.goToPage(this.set.current_page--);
+      }
+    },
 
-  goToNext: function() {
-    if (this.set.current_page < this.set.pages) {
-      this.goToPage(this.set.current_page++);
-    }
-  },
+    // Events supposed to be assigned from outside
+    // onChangePage: function () {
+    //   console.log('Entered page: '+this.getCurentPage());
+    // },
 
-  goToPrev: function() {
-    if (this.set.current_page > 1) {
-      this.goToPage(this.set.current_page--);
-    }
-  },
+    // onFirstPage: function() {
+    //   console.log('First page!');
+    // },
 
-  // Events
-  onChangePage: function () {
-    console.log('Entered page: '+this.getCurentPage());
-  },
+    // onLastPage: function() {
+    //   console.log('Last page!');
+    // }
 
-  onFirstPage: function() {
-    console.log('First page!');
-  },
+  } // Prototype
 
-  onLastPage: function() {
-    console.log('Last page!');
-  }
+  return Constructor;
 
-}
+})();
--- ready.js
+++ ready.js
@@ -1,13 +1,45 @@
+var
+  iP1,
+  iP2;
+
 $(document).ready(function(){
   //La magia aquí
 
-  itemPaginator.onChangePage = function () {
-    console.log('custom'+this.getCurentPage());
-  };
+  // Sample1 ///////////////////////////////////////////////////////////////////
 
-  itemPaginator.init({
+  iP1 = new ItemPaginator({
     items: 283,
     items_per_page: 7,
     container: $('.sample-paginator-1')
   });
+
+  iP1.onChangePage = function () {
+    console.log('custom message '+this.getCurentPage());
+  };
+
+  // Sample2 ///////////////////////////////////////////////////////////////////
+
+  iP2 = new ItemPaginator({
+    items: 46,
+    items_per_page: 3,
+    container: $('#sample-paginator-2'),
+    text: {
+      prev: '&#8249; Anterior',
+      next: 'Siguiente &#8250;'
+    },
+    onFirstPage: function() {
+      console.log( 'sample-2 first page' );
+    },
+    onLastPage: function() {
+      console.log( 'sample-2 last page' );
+    },
+    onChangePage: function() {
+      console.log(this.getCurentPage());
+      
+      // console.log( 'sample-2 entered page: '+this.getCurentPage() );
+    }
+  });
+
+  console.log(iP2);
+
 });
